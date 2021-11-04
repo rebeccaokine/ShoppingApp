a0f0d35a4f13a54c53361164351dcd7c0cf022cf
Naa <77554164+Rebecca-Okine@users.noreply.github.com>
2021-11-04 21:04:11 +0000

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,251 +1,157 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, Image, ScrollView } from 'react-native';
-import { Ionicons, FontAwesome, AntDesign,Feather } from '@expo/vector-icons';
+import { ImageBackground, StyleSheet, Text, View,TouchableOpacity } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 
-export default function Home({navigation}) {
-  return (
-    <ScrollView
-      style={{
-        backgroundColor: '#FFC7FC',
-        flex: 1,
-        paddingTop: 55,
-        paddingHorizontal: 20,
-      }}>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}>
-        <TouchableOpacity>
-        <Ionicons name="ios-menu-outline" size={24} color="purple" />
-        </TouchableOpacity>
+const Login = ({navigation}) => (
+  <View style={styles.container}>
+    <ImageBackground source={{ uri: 'https://i.pinimg.com/564x/2f/ad/28/2fad28ac214b54a0420dbbf103657039.jpg' }} resizeMode="cover" style={styles.image}>
+    
+      <TouchableOpacity 
+     style={{ 
+       justifyContent: 'center', 
+       backgroundColor: "#773091", 
+       borderRadius:10, 
+       padding:5,
+       paddingHorizontal: 20,
+       marginTop:10,
+       opacity:0.5,
+       flexDirection:"row",
+       }}>
 
-        <TouchableOpacity>
-        <AntDesign name="search1" size={20} color="purple" />
-        </TouchableOpacity>
-        
-        
-        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-          <TouchableOpacity 
-          onPress={()=>{navigation.navigate("Cart")}}>
-          <Feather name="shopping-bag" size={20} color="purple" />
-          </TouchableOpacity>
+     <Text style={{fontSize:17,color:"#E5CCFF"}}>Email</Text>
 
-          <TouchableOpacity>
-          <Ionicons
-            style={{ marginLeft: 15 }}
-            name="notifications-outline"
-            size={20}
-            color="purple"
-          />
-          </TouchableOpacity>
-        </View>
-        
+    </TouchableOpacity>
 
-      </View>
+    <TouchableOpacity 
+     style={{ 
+       justifyContent: 'center',
+       backgroundColor: "#773091", 
+       borderRadius:10, 
+       padding:5,
+       paddingHorizontal: 20,
+       marginTop:10,
+       opacity:0.5,
+       flexDirection:"row",
+       }}>
 
+     <Text style={{fontSize:17,color:"#E5CCFF"}}>Password</Text>
+    </TouchableOpacity>
+    
+    <TouchableOpacity>
+    <Text style={{
+      alignItems:"center",
+      color:"#9933FF",
+      fontWeight:500,
+      marginTop:5,
+      opacity:0.5,
+      textAlign:'center'        
+      }}>Forgot Password?</Text>
+    </TouchableOpacity>
 
-      <Text style={{ fontWeight: 'boldest', marginTop: 20,marginBottom: 20,color:'purple' }}>Categories</Text>
+    <TouchableOpacity 
+    onPress={()=>{
+    navigation.navigate("Home")
+    }}
+     style={{ 
+       justifyContent: 'center',
+       backgroundColor: "#773091", 
+       borderRadius:10, 
+       padding:5,
+       paddingHorizontal: 20,
+       marginTop:20,
+       opacity:0.5,
+       flexDirection:"row",
+       }}>
 
+     <Text style={{fontSize:17,color:"#E5CCFF"}}>Login</Text>
+    </TouchableOpacity>
 
+     <Text style={{
+       alignItems:"center",
+      color:"#9933FF",
+      fontWeight:500,
+      marginTop:20,
+      textAlign:'center',
+      }}>or</Text>
+    
+     <Text style={{fontSize:18,
+     color:"#E5CCFF",
+     fontFamily: 'Cochin',
+     textAlign:"center",
+     marginTop:20,
+     }}>Continue with </Text>
+    
 
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-        <View
-          style={{
-            backgroundColor: 'black',
-            padding: 10,
-            paddingHorizontal: 15,
-            borderRadius: 12,
-          }}>
-          <TouchableOpacity>
-            <Text style={{ color: 'purple' }}>All</Text>
-          </TouchableOpacity>
-        </View>
+  <TouchableOpacity 
+     style={{ 
+       justifyContent: 'center',
+       borderRadius:10, 
+       padding: 10,
+       paddingHorizontal: 20,
+       marginTop:10,
+       flexDirection:"row",
+       backgroundColor:"black",
+       opacity:0.7,
+       }}>
+  <AntDesign name ="apple1" size={24} color="#B266FF"/>
+    </TouchableOpacity>
+  
+  <TouchableOpacity 
+     style={{ 
+       justifyContent: 'center',
+       borderRadius:10, 
+       padding: 10,
+       paddingHorizontal: 20,
+       marginTop:10,
+       flexDirection:"row",
+       backgroundColor:"black",
+       opacity:0.7,
+       }}>
+  <AntDesign name ="google" size={24} color="#B266FF"/>
+    </TouchableOpacity>
 
-        <View
-          style={{ backgroundColor: 'black', padding: 10, borderRadius: 12 }}>
-          <TouchableOpacity>
-            <Text style={{ color: 'purple' }}>Tops</Text>
-          </TouchableOpacity>
-        </View>
+  <TouchableOpacity 
+     style={{ 
+       justifyContent: 'center',
+       borderRadius:10, 
+       padding: 10,
+       paddingHorizontal: 20,
+       marginTop:10,
+       flexDirection:"row",
+       backgroundColor:"black",
+       opacity:0.7,
+       }}>
+  <AntDesign name ="facebook-square" size={24} color="#B266FF"/>
+    </TouchableOpacity>
+  
+   <Text style={{
+      marginTop:15,
+      fontWeight:500,
+      color:"#E5CCFF",
+      textAlign:'center',
+    }}
+    >Don't have an account? 
+    <TouchableOpacity onPress={()=>{navigation.navigate('Signup')}}>
+    <Text style={{color: "#B266FF",textAlign:'center'}}> Sign Up</Text>
+    </TouchableOpacity>
+    </Text>
+    
+    
 
-        <View
-          style={{ backgroundColor: 'black', padding: 10, borderRadius: 12 }}>
-          <TouchableOpacity>
-            <Text style={{ color: 'purple' }}>Pants</Text>
-          </TouchableOpacity>
-        </View>
+    </ImageBackground>
+  </View>
+);
 
-        <View
-          style={{ backgroundColor: 'black', padding: 10, borderRadius: 12,opacity:0.7}}>
-          <TouchableOpacity>
-            <Text style={{ color: 'purple' }}>Dresses</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  
+});
 
-      <View
-        style={{
-          marginTop: 20,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <View style={{ flexDirection: 'column', marginVertical: 10 }}>
-
-          <View
-            style={{
-              backgroundColor: 'black',
-              width: 150,
-              height: 230,
-              borderRadius: 15,
-              padding: 15,
-              margin: 10,
-            }}>
-            <Image
-              style={{ height: 150, width: 110, alignSelf: 'center',borderRadius: 15, }}
-              source={{uri: "https://i.pinimg.com/236x/1e/69/9d/1e699de2bca423ed14ae4e9603edef4d.jpg"}}
-            />
-
-            <TouchableOpacity>
-            <Feather style={{textAlign:'right'}}
-            name="shopping-bag"
-             size={20} 
-             color="purple" />
-            </TouchableOpacity>
-
-            <Text style={{ color: '#828080', textAlign: 'center' }}>
-              Pinarello Bike
-            </Text>
-            <Text style={{ fontWeight: 'bolder', textAlign: 'center' }}>
-              <Text style={{ color: 'orange', textAlign: 'center' }}>$</Text>{' '}
-              1,700.00
-            </Text>
-
-          </View>
-          
-          <View
-            style={{
-              backgroundColor: '#f4f2f2',
-              width: 150,
-              height: 230,
-              borderRadius: 15,
-              padding: 15,
-              margin: 10,
-            }}>
-            <Image
-              style={{ height: 150, width: 110, alignSelf: 'center',borderRadius: 15, }}
-              source={{uri: "https://i.pinimg.com/236x/1e/69/9d/1e699de2bca423ed14ae4e9603edef4d.jpg"}}
-            />
-            <TouchableOpacity>
-            <Feather style={{textAlign:'right'}}
-            name="shopping-bag"
-             size={20} 
-             color="purple" />
-            </TouchableOpacity>
-
-            <Text style={{ color: '#828080', textAlign: 'center' }}>
-              Pinarello Bike
-            </Text>
-            <Text style={{ fontWeight: 'bolder', textAlign: 'center' }}>
-              <Text style={{ color: 'orange', textAlign: 'center' }}>$</Text>{' '}
-              1,700.00
-            </Text>
-          </View>
-
-           <View
-            style={{
-              backgroundColor: '#f4f2f2',
-              width: 150,
-              height: 230,
-              borderRadius: 15,
-              padding: 15,
-              margin: 10,
-            }}>
-            <Image
-              style={{ height: 150, width: 110, alignSelf: 'center',borderRadius: 15, }}
-              source={{uri: "https://i.pinimg.com/236x/1e/69/9d/1e699de2bca423ed14ae4e9603edef4d.jpg"}}
-            />
-            <TouchableOpacity>
-            <Feather style={{textAlign:'right'}}
-            name="shopping-bag"
-             size={20} 
-             color="purple" />
-            </TouchableOpacity>
-
-            <Text style={{ color: '#828080', textAlign: 'center' }}>
-              Pinarello Bike
-            </Text>
-            <Text style={{ fontWeight: 'bolder', textAlign: 'center' }}>
-              <Text style={{ color: 'orange', textAlign: 'center' }}>$</Text>{' '}
-              1,700.00
-            </Text>
-          </View>
-
-          <View
-            style={{
-              backgroundColor: '#f4f2f2',
-              width: 150,
-              height: 230,
-              borderRadius: 15,
-              padding: 15,
-              margin: 10,
-            }}>
-            <Image
-              style={{ height: 150, width: 110, alignSelf: 'center',borderRadius: 15, }}
-              source={{uri: "https://i.pinimg.com/236x/1e/69/9d/1e699de2bca423ed14ae4e9603edef4d.jpg"}}
-            />
-            <TouchableOpacity>
-            <Feather style={{textAlign:'right'}}
-            name="shopping-bag"
-             size={20} 
-             color="purple" />
-            </TouchableOpacity>
-
-            <Text style={{ color: '#828080', textAlign: 'center' }}>
-              Pinarello Bike
-            </Text>
-            <Text style={{ fontWeight: 'bolder', textAlign: 'center' }}>
-              <Text style={{ color: 'orange', textAlign: 'center' }}>$</Text>{' '}
-              1,700.00
-            </Text>
-          </View>
-
-          <View
-            style={{
-              backgroundColor: '#f4f2f2',
-              width: 150,
-              height: 230,
-              borderRadius: 15,
-              padding: 15,
-              margin: 10,
-            }}>
-            <Image
-              style={{ height: 150, width: 110, alignSelf: 'center',borderRadius: 15, }}
-              source={{uri: "https://i.pinimg.com/236x/1e/69/9d/1e699de2bca423ed14ae4e9603edef4d.jpg"}}
-            />
-
-            <TouchableOpacity>
-            <Feather style={{textAlign:'right'}}
-            name="shopping-bag"
-             size={20} 
-             color="purple" />
-            </TouchableOpacity>
-
-            <Text style={{ color: '#828080', textAlign: 'center' }}>
-              Pinarello Bike
-            </Text>
-            <Text style={{ fontWeight: 'bolder', textAlign: 'center' }}>
-              <Text style={{ color: 'orange', textAlign: 'center' }}>$</Text>{' '}
-              1,700.00
-            </Text>
-          </View>
-
-
-
-          
-        </View>
-      </View>
-    </ScrollView>
-  );
-}
+export default Login;
