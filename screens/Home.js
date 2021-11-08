1e5510129a2b06c842ac600c747157bd7df5955a
Naa <rnaokine002@st.ug.edu.gh>
2021-11-08 17:32:44 +0000

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -11,6 +11,7 @@ export default function Home({navigation}) {
         paddingTop: 55,
         paddingHorizontal: 20,
       }}>
+
       <View
         style={{
           flexDirection: 'row',
@@ -41,49 +42,58 @@ export default function Home({navigation}) {
           />
           </TouchableOpacity>
         </View>
-        
-
       </View>
 
 
-      <Text style={{ fontWeight: 'boldest', marginTop: 20,marginBottom: 20,color:'purple' }}>Categories</Text>
+      <Text style=
+      {{ fontWeight: '700',
+       marginTop: 20,
+       marginBottom: 20,
+       color:'purple',
+       fontSize: 20
+      }}>Categories</Text>
 
 
 
       <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
         <View
           style={{
-            backgroundColor: 'black',
+            backgroundColor: 'white',
             padding: 10,
             paddingHorizontal: 15,
             borderRadius: 12,
           }}>
           <TouchableOpacity>
-            <Text style={{ color: 'purple' }}>All</Text>
+            <Text style={{
+               color: 'purple'
+             }}>
+             All</Text>
           </TouchableOpacity>
         </View>
 
         <View
-          style={{ backgroundColor: 'black', padding: 10, borderRadius: 12 }}>
+          style={{ backgroundColor: 'white', padding: 10, borderRadius: 12 }}>
           <TouchableOpacity>
             <Text style={{ color: 'purple' }}>Tops</Text>
           </TouchableOpacity>
         </View>
 
         <View
-          style={{ backgroundColor: 'black', padding: 10, borderRadius: 12 }}>
+          style={{ backgroundColor: 'white', padding: 10, borderRadius: 12 }}>
           <TouchableOpacity>
             <Text style={{ color: 'purple' }}>Pants</Text>
           </TouchableOpacity>
         </View>
 
         <View
-          style={{ backgroundColor: 'black', padding: 10, borderRadius: 12,opacity:0.7}}>
+          style={{ backgroundColor: 'white', padding: 10, borderRadius: 12,}}>
           <TouchableOpacity>
             <Text style={{ color: 'purple' }}>Dresses</Text>
           </TouchableOpacity>
         </View>
       </View>
+
+
 
       <View
         style={{
@@ -91,155 +101,224 @@ export default function Home({navigation}) {
           justifyContent: 'center',
           alignItems: 'center',
         }}>
-        <View style={{ flexDirection: 'column', marginVertical: 10 }}>
+        <View style={{ flexDirection: 'row', marginVertical: 10 }}>
 
+
+         <View>
           <View
             style={{
-              backgroundColor: 'black',
+              backgroundColor: 'white',
               width: 150,
-              height: 230,
+              height: 280,
               borderRadius: 15,
               padding: 15,
               margin: 10,
             }}>
             <Image
               style={{ height: 150, width: 110, alignSelf: 'center',borderRadius: 15, }}
-              source={{uri: "https://i.pinimg.com/236x/1e/69/9d/1e699de2bca423ed14ae4e9603edef4d.jpg"}}
+              source={{uri: "https://i.pinimg.com/564x/9c/22/94/9c2294022beb041909d735fb60a8968e.jpg"}}
             />
-
-            <TouchableOpacity>
-            <Feather style={{textAlign:'right'}}
-            name="shopping-bag"
-             size={20} 
-             color="purple" />
-            </TouchableOpacity>
-
+            <View
+              style={{
+                marginTop: 15,
+                backgroundColor: '#FACCFF',
+                alignSelf: 'center',
+                borderRadius: 10,
+                paddingVertical: 5,
+                paddingHorizontal: 20,
+              }}>
+              <TouchableOpacity>
+                <Text
+                  style={{
+                    color: 'purple',
+                    fontWeight: 'bold',
+                    textAlign: 'center',
+                  }}>
+                  Add to Cart
+                  <Feather style={{marginLeft:5}}
+                  name="shopping-bag"
+                  size={20} 
+                  color="purple" 
+                 />
+                </Text>
+              </TouchableOpacity>
+            </View>
             <Text style={{ color: '#828080', textAlign: 'center' }}>
-              Pinarello Bike
+              Orange camo dress
             </Text>
             <Text style={{ fontWeight: 'bolder', textAlign: 'center' }}>
               <Text style={{ color: 'orange', textAlign: 'center' }}>$</Text>{' '}
-              1,700.00
-            </Text>
-
-          </View>
-          
-          <View
-            style={{
-              backgroundColor: '#f4f2f2',
-              width: 150,
-              height: 230,
-              borderRadius: 15,
-              padding: 15,
-              margin: 10,
-            }}>
-            <Image
-              style={{ height: 150, width: 110, alignSelf: 'center',borderRadius: 15, }}
-              source={{uri: "https://i.pinimg.com/236x/1e/69/9d/1e699de2bca423ed14ae4e9603edef4d.jpg"}}
-            />
-            <TouchableOpacity>
-            <Feather style={{textAlign:'right'}}
-            name="shopping-bag"
-             size={20} 
-             color="purple" />
-            </TouchableOpacity>
-
-            <Text style={{ color: '#828080', textAlign: 'center' }}>
-              Pinarello Bike
-            </Text>
-            <Text style={{ fontWeight: 'bolder', textAlign: 'center' }}>
-              <Text style={{ color: 'orange', textAlign: 'center' }}>$</Text>{' '}
-              1,700.00
+              69
             </Text>
           </View>
 
+           
            <View
             style={{
-              backgroundColor: '#f4f2f2',
+              backgroundColor: 'white',
               width: 150,
-              height: 230,
+              height: 280,
               borderRadius: 15,
               padding: 15,
               margin: 10,
+              
             }}>
             <Image
               style={{ height: 150, width: 110, alignSelf: 'center',borderRadius: 15, }}
-              source={{uri: "https://i.pinimg.com/236x/1e/69/9d/1e699de2bca423ed14ae4e9603edef4d.jpg"}}
+              source={{uri: "https://i.pinimg.com/736x/12/20/fc/1220fce979ad0d3f963852fec99f06c8.jpg"}}
             />
-            <TouchableOpacity>
-            <Feather style={{textAlign:'right'}}
-            name="shopping-bag"
-             size={20} 
-             color="purple" />
-            </TouchableOpacity>
+            <View
+              style={{
+                marginTop: 15,
+                backgroundColor: '#FACCFF',
+                alignSelf: 'center',
+                borderRadius: 10,
+                paddingVertical: 5,
+                paddingHorizontal: 20,
+              }}>
+              <TouchableOpacity>
+                <Text
+                  style={{
+                    color: 'purple',
+                    fontWeight: 'bold',
+                    textAlign: 'center',
+                    
+          
+                  }}>
+                  Add to Cart
+                  <Feather style={{marginLeft:5}}
+                  name="shopping-bag"
+                  size={20} 
+                  color="purple" 
+                 />
+                </Text>
+              </TouchableOpacity>
+            </View>
 
             <Text style={{ color: '#828080', textAlign: 'center' }}>
-              Pinarello Bike
+              Something
             </Text>
             <Text style={{ fontWeight: 'bolder', textAlign: 'center' }}>
               <Text style={{ color: 'orange', textAlign: 'center' }}>$</Text>{' '}
-              1,700.00
+              77
             </Text>
           </View>
+          </View>
 
-          <View
+          
+           <View>
+           <View
             style={{
-              backgroundColor: '#f4f2f2',
+              backgroundColor: 'white',
               width: 150,
-              height: 230,
+              height: 280,
               borderRadius: 15,
               padding: 15,
               margin: 10,
             }}>
             <Image
               style={{ height: 150, width: 110, alignSelf: 'center',borderRadius: 15, }}
-              source={{uri: "https://i.pinimg.com/236x/1e/69/9d/1e699de2bca423ed14ae4e9603edef4d.jpg"}}
+              source={{uri: "https://i.pinimg.com/236x/1d/b3/b2/1db3b21eb88f7a133e0d6a8cba45d370.jpg"}}
             />
-            <TouchableOpacity>
-            <Feather style={{textAlign:'right'}}
-            name="shopping-bag"
-             size={20} 
-             color="purple" />
-            </TouchableOpacity>
+            <View
+              style={{
+                marginTop: 15,
+                backgroundColor: '#FACCFF',
+                alignSelf: 'center',
+                borderRadius: 10,
+                paddingVertical: 5,
+                paddingHorizontal: 20,
+              }}>
+              <TouchableOpacity>
+                <Text
+                  style={{
+                    color: 'purple',
+                    fontWeight: 'bold',
+                    textAlign: 'center',
+          
+                  }}>
+                  Add to Cart
+                  <Feather style={{marginLeft:5}}
+                  name="shopping-bag"
+                  size={20} 
+                  color="purple" 
+                 />
+                </Text>
+              </TouchableOpacity>
+            </View>
 
             <Text style={{ color: '#828080', textAlign: 'center' }}>
-              Pinarello Bike
+              Something
             </Text>
             <Text style={{ fontWeight: 'bolder', textAlign: 'center' }}>
               <Text style={{ color: 'orange', textAlign: 'center' }}>$</Text>{' '}
-              1,700.00
+              100
             </Text>
           </View>
-
-          <View
+          
+           
+           <View
             style={{
-              backgroundColor: '#f4f2f2',
+              backgroundColor: 'white',
               width: 150,
-              height: 230,
+              height: 280,
               borderRadius: 15,
               padding: 15,
               margin: 10,
             }}>
             <Image
               style={{ height: 150, width: 110, alignSelf: 'center',borderRadius: 15, }}
-              source={{uri: "https://i.pinimg.com/236x/1e/69/9d/1e699de2bca423ed14ae4e9603edef4d.jpg"}}
+              source={{uri: "https://i.pinimg.com/236x/8e/27/10/8e27108ce759ac221e0de1e6bebcd1aa.jpg"}}
             />
-
-            <TouchableOpacity>
-            <Feather style={{textAlign:'right'}}
-            name="shopping-bag"
-             size={20} 
-             color="purple" />
-            </TouchableOpacity>
+            <View
+              style={{
+                marginTop: 15,
+                backgroundColor: '#FACCFF',
+                alignSelf: 'center',
+                borderRadius: 10,
+                paddingVertical: 5,
+                paddingHorizontal: 20,
+              }}>
+              <TouchableOpacity>
+                <Text
+                  style={{
+                    color: 'purple',
+                    fontWeight: 'bold',
+                    textAlign: 'center',
+                    
+                  }}>
+                  Add to Cart
+                  <Feather style={{marginLeft:5}}
+                  name="shopping-bag"
+                  size={20} 
+                  color="purple" 
+                 />
+                </Text>
+              </TouchableOpacity>
+            </View>
 
             <Text style={{ color: '#828080', textAlign: 'center' }}>
-              Pinarello Bike
+              Something
             </Text>
             <Text style={{ fontWeight: 'bolder', textAlign: 'center' }}>
               <Text style={{ color: 'orange', textAlign: 'center' }}>$</Text>{' '}
-              1,700.00
+              42
             </Text>
+
           </View>
+          </View>
+
+          
+
+          
+
+          
+          
+          
+    
+             
+
+         
 
 
 
