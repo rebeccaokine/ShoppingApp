4596649d4d30a40f3c23e4c07654050b470a3b73
Naa <rnaokine002@st.ug.edu.gh>
2021-11-17 19:33:15 +0000

    Update Splash.js

--- screens/Splash.js
+++ screens/Splash.js
@@ -2,38 +2,29 @@ import React from 'react';
 import { ImageBackground, StyleSheet, Text, View,TouchableOpacity} from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 
-const image = { uri: 'https://i.pinimg.com/564x/21/03/08/210308d8947d0ffb1b51280dcf617d4c.jpg' };
+const image = { uri: 'https://i.pinimg.com/564x/c1/0b/66/c10b66b6de3018c1e15510bbb8ba8908.jpg' };
 
 const SplashScreen = ({navigation}) => (
   <View style={styles.container}>
     <ImageBackground source={image} resizeMode="cover" style={styles.image}>
 
     <TouchableOpacity 
-    onPress={()=>{navigation.navigate("Login")}}
-       style={{
-          textAlign:'center',
-          flexDirection:"row",
-          backgroundColor:'black',
-          justifyContent:'center',
-          opacity:0.5
-       }}>
-        <Text style={{color:"#E5CCFF",marginRight:10,textAlign:'center',fontSize:18}}>Get Started</Text>
-        <AntDesign name ="forward" size={24} color="#B266FF"/>
+    onPress={()=>{navigation.navigate("StartScreen")}}
+       style={styles.TouchableOpacity}>
+        <Text style={{color:"#FFE5CC",marginRight:10,textAlign:'center',fontSize:25,fontWeight:'bold'}}>Get Started</Text>
+        <AntDesign name ="forward" size={30} color="#ff9933"/>
         </TouchableOpacity>
 
       <Text style={styles.text}>Dainty</Text>
       <Text style={{
         fontSize:20,
-        color:'#E5CCFF',
+        color:'#FFE5CC',
         textAlign:'center',
         fontFamily:'papyrus',
         marginLeft:20,
         marginRight:10
         }}>by Rebecca
         </Text>
-
-        
-
     </ImageBackground>
   </View>
 );
@@ -47,13 +38,24 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   text: {
-    marginTop:500,
-    color: '#E5CCFF',
+    marginTop:10,
+    color: '#FFE5CC',
     fontFamily: 'Brush Script MT',
     fontSize: 50,
     fontWeight: '700',
     textAlign: 'center',
   },
+    TouchableOpacity:{
+          textAlign:'center',
+          flexDirection:"row",
+          backgroundColor:'black',
+          justifyContent:'center',
+          opacity:0.7,
+          borderWidth:10,
+          borderRadius:30,
+          marginHorizontal:50,
+          marginTop:500,
+    },
 });
 
 export default SplashScreen;
